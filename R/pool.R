b3b8331f226de40beda80651c84fad9a1f340f3a
Roy Ratcliffe <royratcliffe@me.com>
2021-06-26 07:53:49 +0100

    Wrap long lines

--- R/pool.R
+++ R/pool.R
@@ -2,9 +2,12 @@ db.pool <- NULL
 
 #' Default PostgreSQL database pool
 #'
-#' Lazily creates a default pool for PostgreSQL connections. Call this first at some point in your application's initialisation procedure. Raises an error if a new default pool cannot connect to the database.
+#' Lazily creates a default pool for PostgreSQL connections. Call this first at
+#' some point in your application's initialisation procedure. Raises an error if
+#' a new default pool cannot connect to the database.
 #'
-#' @param ... Extra arguments for \code{RPostgres::\link[RPostgres]{postgresDefault}}
+#' @param ... Extra arguments for
+#'   \code{RPostgres::\link[RPostgres]{postgresDefault}}
 #' @export
 postgres.default.db.pool <- function(...) {
   if (is.null(db.pool)) db.pool <<- pool::dbPool(RPostgres::postgresDefault(...))
@@ -13,9 +16,11 @@ postgres.default.db.pool <- function(...) {
 
 #' Wait for any PostgreSQL notification
 #'
-#' Uses the default pool. The default pool uses the default PostgreSQL connection.
+#' Uses the default pool. The default pool uses the default PostgreSQL
+#' connection.
 #'
-#' @param ... Extra arguments for PostgreSQL notification wait, \code{RPostgres::\link[RPostgres]{postgresWaitForNotify}}.
+#' @param ... Extra arguments for PostgreSQL notification wait,
+#'   \code{RPostgres::\link[RPostgres]{postgresWaitForNotify}}.
 #'   Available parameters include:
 #'   * timeout Seconds to wait, one by default.
 #' @export
