c4fdbb2c65a8e0b4adb8feccbd572a3d5bb32615
Roy Ratcliffe <royratcliffe@me.com>
2021-06-26 16:44:15 +0100

    Skip tests if no default connection

--- tests/testthat/test-pool.R
+++ tests/testthat/test-pool.R
@@ -1,3 +1,11 @@
+user <- "postgres"
+
+# Could not initialise default postgres database. If postgres is running
+# check that the environment variables PGHOST, PGPORT,
+# PGUSER, PGPASSWORD, and PGDATABASE, are defined and
+# point to your database.
+skip_if_not(suppressMessages(RPostgres::postgresHasDefault(user = user)), "default postgres database required but is not found")
+
 test_that("default works", {
-  expect_type(postgres.default.db.pool(user = "postgres"), "environment")
+  expect_type(postgres.default.db.pool(user = user), "environment")
 })
