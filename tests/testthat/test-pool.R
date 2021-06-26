feaedfe6dc567d0648d04be7cf9e769b450e50ec
Roy Ratcliffe <royratcliffe@me.com>
2021-06-26 16:44:41 +0100

    Improve comment

--- tests/testthat/test-pool.R
+++ tests/testthat/test-pool.R
@@ -1,9 +1,13 @@
 user <- "postgres"
 
-# Could not initialise default postgres database. If postgres is running
-# check that the environment variables PGHOST, PGPORT,
-# PGUSER, PGPASSWORD, and PGDATABASE, are defined and
-# point to your database.
+# Skip the following tests unless the tests can find a PostgreSQL database.
+# Otherwise connection failure triggers the following message.
+#
+#   Could not initialise default postgres database. If postgres is running
+#   check that the environment variables PGHOST, PGPORT,
+#   PGUSER, PGPASSWORD, and PGDATABASE, are defined and
+#   point to your database.
+#
 skip_if_not(suppressMessages(RPostgres::postgresHasDefault(user = user)), "default postgres database required but is not found")
 
 test_that("default works", {
