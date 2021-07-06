f8c01c8d01113091532dcab709eff721ea47890f
Roy Ratcliffe <royratcliffe@me.com>
2021-07-06 20:28:12 +0100

    Database migration

---
+++ R/migrate.R
@@ -0,0 +1,27 @@
+#' Run migrations
+#'
+#' Creates the migration table if it does not already exist. Assumes that the schema already exists; fails otherwise.
+#'
+#' Intersects the migration table with the migration directory. Runs the missing migration scripts in lexicographic order. Implementation relies on \link{DBI} package and its functional interface.
+#'
+#' @param dir Migration directory
+#' @param name Migration table
+#' @export
+migrate <- function(dir, name = DBI::Id(schema = "public", table = "migrations")) {
+  files <- sort(tools::list_files_with_exts(dir, "R"))
+  names(files) <- tools::file_path_sans_ext(basename(files))
+  repeat {
+    migrations <- withTransaction(function(conn) {
+      if (!DBI::dbExistsTable(conn, name))
+        DBI::dbCreateTable(conn, name, fields = c(id = DBI::dbDataType(conn, "")))
+      DBI::dbReadTable(conn, name)$id
+    })
+    diff <- files[setdiff(names(files), migrations)]
+    if (length(diff) == 0) break
+    withTransaction(function(conn) {
+      env <- list2env(list(conn = conn))
+      sys.source(diff[1], envir = env)
+      DBI::dbAppendTable(conn, name, data.frame(id = names(diff)[1]))
+    })
+  }
+}
