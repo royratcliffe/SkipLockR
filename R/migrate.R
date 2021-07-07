9f542d88469a04e2beaebe2f71a6f27665c99f93
Roy Ratcliffe <royratcliffe@me.com>
2021-07-07 06:23:15 +0100

    Wrap lines

--- R/migrate.R
+++ R/migrate.R
@@ -1,13 +1,16 @@
 #' Run migrations
 #'
-#' Creates the migration table if it does not already exist. Assumes that the schema already exists; fails otherwise.
+#' Creates the migration table if it does not already exist. Assumes that the
+#' schema already exists; fails otherwise.
 #'
-#' Intersects the migration table with the migration directory. Runs the missing migration scripts in lexicographic order. Implementation relies on \link{DBI} package and its functional interface.
+#' Intersects the migration table with the migration directory. Runs the missing
+#' migration scripts in lexicographic order. Implementation relies on \link{DBI}
+#' package and its functional interface.
 #'
 #' @param dir Migration directory
 #' @param name Migration table
 #' @export
-migrate <- function(dir, name = DBI::Id(schema = "public", table = "migrations")) {
+migrate <- function(dir, name = DBI::Id(schema = "schema", table = "migrations")) {
   files <- sort(tools::list_files_with_exts(dir, "R"))
   names(files) <- tools::file_path_sans_ext(basename(files))
   repeat {
@@ -19,8 +22,7 @@ migrate <- function(dir, name = DBI::Id(schema = "public", table = "migrations")
     diff <- files[setdiff(names(files), migrations)]
     if (length(diff) == 0) break
     withTransaction(function(conn) {
-      env <- list2env(list(conn = conn))
-      sys.source(diff[1], envir = env)
+      sys.source(diff[1], envir = list2env(list(conn = conn)))
       DBI::dbAppendTable(conn, name, data.frame(id = names(diff)[1]))
     })
   }
